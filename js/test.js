d33c492f53a73847d85ac684dffca00c713e8ef5
Daniel VP <drifta.dvp@gmail.com>
2022-11-24 19:49:35 +0200

    Update 1

---
+++ js/test.js
@@ -0,0 +1,9 @@
+const names = ["Daniel", "Mary", "Johnny", "Sandra"]
+const title = "My Modern Calculator"
+document.getElementById("title").innerText = title
+document.querySelector('ol').innerHTML = `
+    <li>It adds numbers.</li>
+    <li>It subtracts numbers.</li>
+    <li>It multiplies numbers.</li>
+    <li>It divides numbers.</li>
+`
